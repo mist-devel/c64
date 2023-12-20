0ffaba208a5beb9a2da7ebf0badd391dacdf51c8
Gyorgy Szombathelyi <8644936+gyurco@users.noreply.github.com>
2023-12-20 17:52:53 +0100

    VIC-II: reset rasterY to 0 one cycle later

--- rtl/video_vicII_656x_a.vhd
+++ rtl/video_vicII_656x_a.vhd
@@ -144,6 +144,7 @@ architecture rtl of video_vicii_656x is
 	signal rasterY : unsigned(8 downto 0) := (others => '0');
 	signal rasterY_next : unsigned(8 downto 0);
 	signal cycleLast : boolean;
+	signal cycleLastDelay : boolean;
 	signal rasterXDelay : unsigned(9 downto 0);
 
 -- Light pen
@@ -738,7 +739,9 @@ rasterCounters: process(clk, rasterX, rasterXDelay)
 			if phi = '1'
 			and enaData = '1'
 			and baSync = '0' then
-				if cycleLast then
+				cycleLastDelay <= false;
+				if (lastLineFlag) then cycleLastDelay <= cycleLast; end if;
+				if (lastLineFlag and cycleLastDelay) or (not lastLineFlag and cycleLast) then
 					rasterY <= rasterY_next;
 				end if;
 			end if;
@@ -754,8 +757,7 @@ rasterCounters: process(clk, rasterX, rasterXDelay)
 			if phi = '1'
 			and enaData = '1'
 			and baSync = '0'
-			and (vicCycle = cycleSpriteB)
-			and (sprite = 2) then
+			and ((lastLineFlag and cycleLastDelay) or (not lastLineFlag and cycleLast)) then
 				rasterIrqDone <= '0';
 			end if;
 			if resetRasterIrq = '1' then
